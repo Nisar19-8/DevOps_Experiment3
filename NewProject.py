220a21d70902fb8380e37fb2fc04150ab6212b9a
sakshi <sanketkokate1084@gmail.com>
2025-04-07 09:34:27 +0530

    New Project Added

---
+++ NewProject.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+
+i = 1
+is_square = False
+
+while i * i <= num:
+    if i * i == num:
+        is_square = True
+        break
+    i += 1
+
+if is_square:
+    print("Perfect square")
+else:
+    print("Not a perfect square")
+
